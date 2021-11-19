27845f7b01a92a9653e911aa497874cf5f622603
hiteshpakhan <hitesh5pakhan@gmail.com>
2021-11-19 10:31:04 +0530

    a

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-
-
-const apiUrl = "https://api.unsplash.com/photos/random/?client_id=YOUR_ACCESS_KEY&count"
+const count = 10;
+const apiKey = "";
+const apiUrl = 'https://api.unsplash.com/photos/random/?client_id=YOUR_ACCESS_KEY'+ apiKey +'&count='+count;
